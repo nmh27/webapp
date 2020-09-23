0791b54aaddc4a2bb1683fee1da1a83288995e72
shaan nagra <h.s.nagra98@gmail.com>
2020-09-23 22:53:32 +1200

    commented the previous two commits changes

--- php/functions.php
+++ php/functions.php
@@ -39,21 +39,105 @@ function sendResults(){
 
 }
 
-class Frame{
-  public $id;
-  public $hTubeAngle;
-  public $hsetCovr;
-  public $frameX;
-  public $frameY;
+/*
+  FUNCTION: calculateHXY
 
-  public $sStemAngle;
-  public $sStemLength;
-  public $sStemIsFlippable;
+  Parameters: FRAME(class)
+  returns list HXYS for stock stem ONLY! array(array())
+
+  Parameters: FRAME(class), stems(array)
+  returns list of HXYS for each stem (array(array(array()))
+  NOTE: stems(array) input FORMAT is [stemID, Angle, Length, isFlippable]
+*/
+function calculateHXY($f,$stems=null){
+  //Frame Values required for calculating
+  $hTubeAngle = $f->hTubeAngle;
+  $hsetCovr = $f->hsetCovr;
+  $frameX = $f->frameX;
+  $frameY = $f->frameY;
+  $sMin = $f->sMin;
+  $sMax = $f->sMax;
+
+  //if called without PARAM: stems(array)
+  if($stems===null){
+    //ADD the stock stem to an array for calculating
+    $stems = array(array(0,$f->sStemAngle,$f->sStemLength,$f->sStemIsFlippable));
+  }
+
+  $stemHXYList = array();
+  //for all stems in the array calculated HXY
+  foreach ($stems as $s){
+    $stemHXY = array();
+    //for all possible spacing calcluate HXY
+    for($i=$sMin;$i<=$sMax;$i+=5){
+      $sA = $s[1];
+      $sL = $s[2];
+      $isF = $s[3];
+      //while true EXPLAINED in IF BREAK
+      while(true){
+        $flipped = false;//default calculation is not flipped
+
+        $RadAdjStemA = deg2rad($sA + 90 - $hTubeAngle);
+
+  			$RadAdjHeadA = deg2rad(90 - $hTubeAngle);
+
+  			$B = (($hsetCovr+$i)+20-17*tan(deg2rad($sA)));
+
+  			$StemX = $sL * cos($RadAdjStemA);
+  			$spacerDiff = $B * sin($RadAdjHeadA);
+  			$X = $StemX - $spacerDiff;
+
+  			$StemY = $sL * sin($RadAdjStemA);
+  			$spacerH = $B * cos($RadAdjHeadA);
+  			$Y = $StemY + $spacerH;
+
+        $HX = $X+$FrameX;
+        $HY = $Y+$FrameY;
+
+        $stemHXY[]=(array($HX,$HY,$s[0],$sA,$sL,$flipped,$i));
+
+        //if is flippable
+        //repeat loop with it Angle flipped
+        if($isF==true){
+          $flipped = true;
+          $sA = -1*$sA
+          $isF = false;
+        }else {
+          break;
+        }
+      }
+    }
+    $stemHXYList[]=$stemHXY;
+  }
+  //if only one stem just return its possible HXY postions
+  if(1==count($stemHXYList)){
+    return $stemHXYList[0];
+  }
+  //if more than one return an array(array(array()))
+  else {
+    return $stemHXYList;
+  }
+}
+
+/*
+CLASS FRAME
+values from FRAME GEOMETREY
+*/
+class Frame{
+  public $id;//SizeCODE
+  public $hTubeAngle; //Head Tube Angle
+  public $hsetCovr; //Headset Cover
+  public $frameX; //Frame X
+  public $frameY; //Frame Y
+
+  public $sStemAngle; //Stock Stem Angle
+  public $sStemLength; //Stock Stem Length
+  public $sStemIsFlippable; //Stock Stem is Flippable
 
   public $spacerMin;
   public $spacerMax;
 
-  public $stockConfigs = array();
+  public $stockConfigs; //HXY postions using just the stock stem and spacing
 
   function __construct($i,$hTA,$hC,$fX,$fY,$sSA,$sSL,$sSIF,$sMin,$sMax) {
     $id = $i;
@@ -66,65 +150,7 @@ class Frame{
     $sStemIsFlippable = $sSIF;
     $spacerMin = $sMin;
     $spacerMax = $sMax;
-
+    $stockConfigs = calculateHXY($this);
   }
-  function calculateHXY($f,$stems=null){
-    //for frames that match filters
-    //a bike that met other inputs aswell(filters) example
-    $hTubeAngle = $f->hTubeAngle;
-    $hsetCovr = $f->hsetCovr;
-    $frameX = $f->frameX;
-    $frameY = $f->frameY;
-    $sMin = $f->sMin;
-    $sMax = $f->sMax;
-
-    if($stems===null){
-      $stems = array(array(0,$f->sStemAngle,$f->sStemLength,$f->sStemIsFlippable));
-    }
-    //for all stems possible
-    //a stem that fits and meets some more user inputs
-    $stemHXYList = array();
-    foreach ($stems as $s){
-      $stemHXY = array();
-      for($i=$sMin;$i<=$sMax;$i+=5){
-        $sA = $s[1];
-        $sL = $s[2];
-        $isF = $s[3];
-        while(true){
-          $flipped = false;
-    			$RadAdjStemA = deg2rad($sA + 90 - $hTubeAngle);
-
-    			$RadAdjHeadA = deg2rad(90 - $hTubeAngle);
-
-    			$B = (($hsetCovr+$i)+20-17*tan(deg2rad($sA)));
-
-    			$StemX = $sL * cos($RadAdjStemA);
-    			$spacerDiff = $B * sin($RadAdjHeadA);
-    			$X = $StemX - $spacerDiff;
-
-    			$StemY = $sL * sin($RadAdjStemA);
-    			$spacerH = $B * cos($RadAdjHeadA);
-    			$Y = $StemY + $spacerH;
-
-          $HX = $X+$FrameX;
-          $HY = $Y+$FrameY;
-          $stemHXY[]=(array($HX,$HY,$s[0],$sA,$sL,$flipped));
-          if($isF==true){
-            $flipped = true;
-            $sA = -1*$sA
-            $isF = false;
-          }else {
-            break;
-          }
-        }
-      }
-      $stemHXYList[]=$stemHXY;
-    }
-    if(1==count($stemHXYList)){
-      return $stemHXYList[0];
-    }
-    else {
-      return $stemHXYList;
-    }
-  }
+}
 ?>
