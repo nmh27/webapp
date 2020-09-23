5879430924742d385bb259f58f2197e48ab0cb2d
shaan nagra <h.s.nagra98@gmail.com>
2020-09-23 16:46:26 +1200

    removed on calls from html file  e.g onchange=<jsFunction>
    added jquery empty functions the react to events
    added in empty js function callback
    added in empty js function that will intialize some of form

--- js/form.js
+++ js/form.js
@@ -1,3 +1,42 @@
-function formUpdate(){
 
-}
+//https://www.w3schools.com/jquery/default.asp
+//JQUERY FUNCTIONS SHOULD BE PLACED INSIDE THIS FUNCTIONS
+$(function(){
+  // TIP: Basic syntax is: $(selector).action()
+  //        $ sign to define/access jQuery
+  //        (selector) to "query (or find)" HTML elements
+  //        jQuery action() to be performed on the element(s)
+
+  /*
+    FUNCTION: add in the variables for dropdowns and what not
+    i.e. materials -> carbon fiber, aluminium, ..
+  */
+  function createFormData() {
+
+  }
+
+  /*
+    FUNCTION: when form is changed
+  */
+  $("#userinput").change(function five(){
+    alert("testing");
+  });
+
+  /*
+    FUNCTION: when user submits form
+  */
+  $("#userinput").submit(function(){
+    //process form and get data
+    //make into json
+    //send request
+    //call processResponse when data comeback
+  });
+
+  /*
+    FUNCTION: processes the respone json to the user
+    IS CALLBACK FUNCTION
+  */
+  function processResponse(){
+    //
+  }
+});
